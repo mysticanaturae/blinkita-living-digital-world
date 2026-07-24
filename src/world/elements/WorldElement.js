ce831d196719a9e1804ae80cfeba08b8ebff3dc7
mysticanaturae <mysticaholistica888@gmail.com>
2026-07-24 04:05:03 -0600

    🧩 Create World Element foundation

---
+++ src/world/elements/WorldElement.js
@@ -0,0 +1,30 @@
+/*
+==========================================
+BLINKITA METHOD™
+WORLD ELEMENT™
+
+The building blocks
+of every Living World™
+
+Version 1.0
+==========================================
+*/
+
+
+export const WorldElement = {
+
+    id: "",
+
+    type: "",
+
+    name: "",
+
+    description: "",
+
+    content: null,
+
+    connectedElements: [],
+
+    active: false
+
+};
