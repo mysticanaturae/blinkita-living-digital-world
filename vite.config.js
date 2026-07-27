62992efa83f5c2f78e5cc731f70edb6edd0f15ec
mysticanaturae <mysticaholistica888@gmail.com>
2026-07-27 01:41:25 -0600

    Fix GitHub Pages Vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+
+  plugins: [
+    react()
+  ],
+
+
+  base: "/LIVING-DIGITAL-WORLD-APP/"
+
 })
