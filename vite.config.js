021e71ec8454900e97646091b4810e8128fd77d9
mysticanaturae <mysticaholistica888@gmail.com>
2026-07-27 02:43:57 -0600

    Fix Vercel asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
 
-  plugins: [
-    react()
-  ],
+    plugins: [
+        react()
+    ],
 
-  base: "/blinkita-living-digital-world/"
+
+    base: "/"
 
 })
